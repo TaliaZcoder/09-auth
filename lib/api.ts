3f9fcfa20f203e3323b07826fd9a1a4ef021c744
Tetiana Zinovieva <talia.97.it@gmail.com>
2026-04-23 16:23:47 +0200

    refactor api for auth backend

--- lib/api.ts
+++ lib/api.ts
@@ -1,11 +1,8 @@
 import axios from "axios";
 import type { Note, NoteTag } from "@/types/note";
 
-const BASE_URL = "https://notehub-public.goit.study/api";
-
-const token = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
-
-axios.defaults.baseURL = BASE_URL;
+axios.defaults.baseURL = "/api";
+axios.defaults.withCredentials = true;
 
 export interface FetchNotesResponse {
   notes: Note[];
@@ -18,55 +15,89 @@ export interface CreateNoteRequest {
   tag: NoteTag;
 }
 
+/* NOTES */
+
 export const fetchNotes = async (
   page: number,
   search: string,
   tag?: string
 ): Promise<FetchNotesResponse> => {
-  const response = await axios.get("/notes", {
+  const res = await axios.get("/notes", {
     params: {
       page,
       perPage: 12,
       search,
       ...(tag ? { tag } : {}),
     },
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
   });
 
-  return response.data;
+  return res.data;
 };
 
-export const fetchNoteById = async (id: string): Promise<Note> => {
-  const response = await axios.get(`/notes/${id}`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-
-  return response.data;
+export const fetchNoteById = async (
+  id: string
+): Promise<Note> => {
+  const res = await axios.get(`/notes/${id}`);
+  return res.data;
 };
 
 export const createNote = async (
   note: CreateNoteRequest
 ): Promise<Note> => {
-  const response = await axios.post("/notes", note, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-
-  return response.data;
+  const res = await axios.post("/notes", note);
+  return res.data;
 };
 
-export const deleteNote = async (id: string): Promise<Note> => {
-  const response = await axios.delete(`/notes/${id}`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-
-  return response.data;
+export const deleteNote = async (
+  id: string
+): Promise<Note> => {
+  const res = await axios.delete(`/notes/${id}`);
+  return res.data;
 };
 
+/* AUTH */
+
+export const registerUser = async (
+  email: string,
+  password: string
+) => {
+  const res = await axios.post("/auth/register", {
+    email,
+    password,
+  });
+
+  return res.data;
+};
+
+export const loginUser = async (
+  email: string,
+  password: string
+) => {
+  const res = await axios.post("/auth/login", {
+    email,
+    password,
+  });
+
+  return res.data;
+};
+
+export const logoutUser = async () => {
+  await axios.post("/auth/logout");
+};
+
+export const getSession = async () => {
+  const res = await axios.get("/auth/session");
+  return res.data;
+};
+
+export const getMe = async () => {
+  const res = await axios.get("/users/me");
+  return res.data;
+};
+
+export const updateMe = async (data: {
+  username?: string;
+}) => {
+  const res = await axios.patch("/users/me", data);
+  return res.data;
+};
