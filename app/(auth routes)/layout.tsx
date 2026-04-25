4e5b009c67e134bced27e62f3337fb020c2dc7b0
Tetiana Zinovieva <talia.97.it@gmail.com>
2026-04-25 14:18:39 +0200

    fix auth layout refresh

--- app/(auth routes)/layout.tsx
+++ app/(auth routes)/layout.tsx
@@ -1,7 +1,20 @@
+'use client';
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+type Props = {
+  children: React.ReactNode;
+};
+
 export default function AuthLayout({
   children,
-}: {
-  children: React.ReactNode;
-}) {
+}: Props) {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.refresh();
+  }, [router]);
+
   return <>{children}</>;
 }
