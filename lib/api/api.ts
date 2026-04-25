90dd54cd4b975ef67247801b6cfe36693c260c12
Tetiana Zinovieva <talia.97.it@gmail.com>
2026-04-25 16:06:13 +0200

    new Error for api

--- lib/api/api.ts
+++ lib/api/api.ts
@@ -1,10 +1,17 @@
 import axios from "axios";
 
-const baseURL =
-  process.env.NEXT_PUBLIC_API_URL +
-  "/api";
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+if (!API_URL) {
+  throw new Error("NEXT_PUBLIC_API_URL is not defined");
+}
+
+const baseURL = `${API_URL}/api`;
 
 export const api = axios.create({
   baseURL,
   withCredentials: true,
 });
+
+console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
+console.log("BASE URL:", baseURL);
